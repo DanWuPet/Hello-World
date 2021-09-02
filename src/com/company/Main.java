09c6f96aec2df02dfe138d81a9bec86869bbffa0
DanWuPet <danx1698@edu.easj.dk>
2021-09-02 11:28:49 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello World");
+        System.out.println("Counting to 10");
+
+        // Create Variable
+        int i = 0;
+
+        // Simple While loop
+        while(i != 10){
+            System.out.println(i + 1);
+            i = i + 1;
+        }
+    }
+}
